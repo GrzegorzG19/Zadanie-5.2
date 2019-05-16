fa1beaa5452c73f85e9101d2a4cb27abbd02c9b3
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-16 15:47:18 +0200

    Zadanie

---
+++ src/Device.java
@@ -0,0 +1,28 @@
+public class Device {
+
+    String model;
+    String company;
+    String device;
+    double price;
+    String kod;
+
+    public Device(String model, String company, String device, double price) {
+        this.model = model;
+        this.company = company;
+        this.device = device;
+        this.price = price;
+
+    }
+
+    public Device(String model, String company, String device) {
+        this.model = model;
+        this.company = company;
+        this.device = device;
+    }
+
+    String getInfo(){
+         this.kod =("Kod: " + this.model +", Producent: "+ this.company+", typ: " + this.device + ", cena: "+ this.price);
+    return kod;
+    }
+
+}
